e4f6c91032f1b1abe9098afd09421da81493ba14
sergeyfrolenko <sergeyfrolenko@ro.ru>
2020-11-15 06:16:37 +0300

    board create

--- client/src/Game/Board.jsx
+++ client/src/Game/Board.jsx
@@ -1,27 +1,31 @@
 import React from 'react'
 import {Square} from './Square'
+import s from './Game.module.css'
 
 export class Board extends React.Component {
-  const n = 0
-  renderRow(num) {
-    return (
-      <div className="row">
-        <Square value={num} />    
-      </div>
-    )
-  }
-  renderBoard() {
-    let result = []
-    for(let i=0; i<3; i++) {
-      result.push(this.renderRow(i))
-    }
-    return result
+  renderSquare(num) {
+    return <Square value={num} />
   }
 
   render() {
     return (
-      <div>
-        {this.renderBoard()}  
+      <div className={s.game}>
+        <h1>Game</h1>
+        <div className={s.row}>
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+          {this.renderSquare(3)}
+        </div>
+        <div className={s.row}>
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+          {this.renderSquare(6)}
+        </div>
+        <div className={s.row}>
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+          {this.renderSquare(9)}
+        </div>
       </div>
     )
   }
